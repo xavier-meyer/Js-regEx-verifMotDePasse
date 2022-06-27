2120f3ebb82a9b92685f6bd8633bd17712bd60eb
xavier-meyer <xavier.01@orange.fr>
2022-06-27 17:56:19 +0200

    feat:add checklist for password  verification HTML and JS file

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 
 const passwordInput = document.getElementById("passWord");
 
+const liMin = document.getElementById("li-min");
+const liMaj = document.getElementById("li-maj");
+const liNumber = document.getElementById("li-number");
+const liCaracterSpecial = document.getElementById("li-caracterSpecial");
+const liCaractersTotal = document.getElementById("li-caractersTotal");
+
+const avertissement = document.getElementById("avertissement");
+
 const regFort = /^(?=.{12,}$)(?=.*?[a-z]){1}(?=.*?[A-Z]){1}(?=.*?[0-9]){1}(?=.*\W).*$/;
 const regMajuscule = /[A-Z]/;
 const regMinuscule = /[a-z]/;
 const regNumber = /[0-9]/;
 const regCharacter = /\W/;
 
-
-const avertissement = document.getElementById("avertissement");
-
 passwordInput.addEventListener("input", () => {
     console.log(passwordInput.value);
     avertissement.textContent = "Votre mot de passe est invalide";
-    setTimeout(() => {
-                avertissement.textContent = "";
-            },4000)
-    if((regMajuscule.test(passwordInput.value))||(regMinuscule.test(passwordInput.value))){
+    if((regMajuscule.test(passwordInput.value))){
         avertissement.textContent = "Votre mot de passe est faible";
-        setTimeout(() => {
-            avertissement.textContent = "";
-        },4000)
-    }   
-        if(regNumber.test(passwordInput.value)){
-            avertissement.textContent = "Votre mot de passe est moyen";
-            setTimeout(() => {
-                avertissement.textContent = "";
-            },4000)
-        } 
-            if(regCharacter.test(passwordInput.value)){
-                avertissement.textContent = "Votre mot de passe est fort";
-                setTimeout(() => {
-                    avertissement.textContent = "";
-                },4000) 
-            }
-                if(regFort.test(passwordInput.value)){
-                    avertissement.textContent = "Votre mot de passe est sécurisé";
-                    setTimeout(() => {
-                        avertissement.textContent = "";
-                    },4000) 
-                }
+        liMaj.style.color = "orange";
+    }    
+        if(regMinuscule.test(passwordInput.value)){
+            avertissement.textContent = "Votre mot de passe est faible";
+            liMin.style.color = "orange";
+        }    
+            if(regNumber.test(passwordInput.value)){
+                avertissement.textContent = "Votre mot de passe est moyen";
+                liNumber.style.color = "orange";
+            }    
+                if(regCharacter.test(passwordInput.value)){
+                    avertissement.textContent = "Votre mot de passe est fort";
+                    liCaracterSpecial.style.color = "orange";
+                }    
+                    if(regFort.test(passwordInput.value)){
+                        avertissement.textContent = "Votre mot de passe est sécurisé";
+                        liCaractersTotal.style.color = "orange";
+                    }
+           
 })  
 
 
